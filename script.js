dd2baf453d298a2635e5699c0790bde35b708d61
Jonatan-Matwijow <jonatanmatwijow@gmail.com>
2023-04-05 19:35:28 -0300

    Arreglado de secciones y medias queries

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //btn volver al inicio
-/*Estas lineas serian del boton para volver al inicio, reveer si va a estar o no.
+/*Estas lineas serian del boton para volver al inicio, reveer si va a estar o no*/
 
-document.getElementById("btn_scroll").addEventListener("click", scrollUp);
+/*document.getElementById("btn_scroll").addEventListener("click", scrollUp);
 
-let btnUp = document.getElementById("btn_scroll");
 
 function scrollUp(){
   let currentScroll = document.documentElement.scrollTop;
@@ -11,27 +10,29 @@ function scrollUp(){
   if (currentScroll > 0){
     window.requestAnimationFrame(scrollUp);
     windows.scrollTo(0, currentScroll - (currentScroll / 10));
+    btnUp.style.transform = "scale(0)";
+    
   }
 }
 
-window.onscroll =function(){
-let scroll = document.documentElement.scrollTop;
+btnUp = document.getElementById("btn_scroll");
 
-if (scroll > 100){
-  btnUp.style.transform = "scale(1)"
-}
-  else if (scroll < 100){
-    btnUp.style.transform = "scale(0)";
-}
-}
-*/
+window.onscroll = function(){
+  let scroll = document.documentElement.scrollTop;
+
+  if (scroll > 5000){
+    btnUp.style.transform = "scale(1)"
+    }
+    else if (scroll < 500){
+     btnUp.style.transform = "scale(0)";
+    }
+}*/
 
 //Lineas del modal de los certificados
 
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal'), () => 
-{
- myInput.focus();
-}
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
