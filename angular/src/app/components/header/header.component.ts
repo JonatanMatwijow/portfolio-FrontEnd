12dc2e3e88b39f8d9c4a5722ad368c3f830c5de1
Jonatan-Matwijow <jonatanmatwijow@gmail.com>
2023-05-15 16:51:59 -0300

    trabajando los servicios

--- angular/src/app/components/header/header.component.ts
+++ angular/src/app/components/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/servicios/porfolio.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  miPorfolio:any;
+
+  constructor(private datosporfolio: PorfolioService) {}
+  ngOnInit(): void {
+    this.datosporfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.miPorfolio=data;
+    });
+  }
 }
